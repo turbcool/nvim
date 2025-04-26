e3be1c7abb898a77775043d2def17559db105f18
Ilya Naidanov <turbcool@gmail.com>
2025-04-26 15:44:13 +0500

    Themes

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -7,6 +5,18 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+
+  -- Themes:
+  { import = "astrocommunity.colorscheme.vscode-nvim" },
+  { import = "astrocommunity.colorscheme.sonokai" },
+  { import = "astrocommunity.color.transparent-nvim" },
+
+  -- Editor + utils:
+  { import = "astrocommunity.editing-support.undotree" },
+  { import = "astrocommunity.editing-support.vim-move" },
+  { import = "astrocommunity.docker.lazydocker" },
+  { import = "astrocommunity.note-taking.neorg" },
+  { import = "astrocommunity.note-taking.global-note-nvim" },
+
+
 }
