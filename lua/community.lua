69cb27ee287b5ff2aa070bd7c5dc8c583d41be5c
Ilya Naidanov <turbcool@gmail.com>
2025-04-28 19:46:59 +0500

    Fixes

--- lua/community.lua
+++ lua/community.lua
@@ -15,8 +15,13 @@ return {
   { import = "astrocommunity.editing-support.undotree" },
   { import = "astrocommunity.editing-support.vim-move" },
   { import = "astrocommunity.docker.lazydocker" },
-  { import = "astrocommunity.note-taking.neorg" },
+  --{ import = "astrocommunity.note-taking.neorg" },
   { import = "astrocommunity.note-taking.global-note-nvim" },
+
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.code-runner.overseer-nvim" },
 
 
 }
