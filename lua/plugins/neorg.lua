c30d3e7fb4d4a6d0db8905b6115468da8796e877
Ilya Naidanov <turbcool@gmail.com>
2026-03-03 12:31:28 +0500

    Disabled neorg

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,64 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+return {
+  {
+    "benlubas/neorg-interim-ls",
+    ft = "norg", -- lazy load on norg files
+    -- optional: no config here, Neorg will configure it
+  },
+
+  -- override neorg opts coming from the astrocommunity module
+  {
+    "nvim-neorg/neorg",
+    dependencies = { "nvim-neorg/tree-sitter-norg" },
+    opts = function(_, opts)
+      local astrocore = require("astrocore")
+      opts = opts or {}
+
+      return astrocore.extend_tbl(opts, {
+        -- ensure the interim-ls external module is loaded and configure it
+        load = {
+          ["external.interim-ls"] = {
+            config = {
+              completion_provider = {
+                enable = true,
+                documentation = true,
+                categories = false,
+                people = {
+                  enable = false,
+                  path = "people",
+                },
+              },
+            },
+          },
+
+          -- tell Neorg to use the external lsp-completion module
+          ["core.completion"] = {
+            config = {
+              engine = { module_name = "external.lsp-completion" },
+            },
+          },
+
+          -- keep any other modules you already use (example)
+          ["core.defaults"] = {},
+          ["core.concealer"] = {},
+          ["core.keybinds"] = {},
+          ["core.journal"] = {
+            config = {
+              workspace = "notes",
+              strategy = "flat",
+            },
+          },
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                notes = "~/projects/notes",
+              },
+              default_workspace = "notes",
+            },
+          },
+        },
+      })
+    end,
+  },
+}
